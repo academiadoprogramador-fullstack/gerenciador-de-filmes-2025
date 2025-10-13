20110561cbc0e8aff31a08b0795f92748ef5ff84
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-13 14:27:02 -0300

    Adicionado banner principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { BannerPrincipal } from './components/banner-principal/banner-principal';
 import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, BannerPrincipal],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
