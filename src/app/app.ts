c97345a392f8ef8545637ea7d0fa8643b2baf076
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-16 10:52:20 -0300

    Adicionada barra de busca

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { BarraBusca } from './components/barra-busca/barra-busca';
 import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, BarraBusca],
   templateUrl: './app.html',
 })
 export class App {}
