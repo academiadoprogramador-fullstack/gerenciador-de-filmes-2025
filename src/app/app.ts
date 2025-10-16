d537dff78bacc5534c22c69a6fbee6e1cd9b9956
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-10-16 10:53:32 -0300

    Movido componente Navbar para shared

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { BarraBusca } from './components/barra-busca/barra-busca';
-import { Navbar } from './components/navbar/navbar';
+import { Navbar } from './components/shared/navbar/navbar';
 
 @Component({
   selector: 'app-root',
